6143c5cdbc5efbcf07e843fc574342faccb65560
ksantr <anton@avanan.com>
2018-06-17 13:19:29 +0300

    Push all

---
+++ README.md
@@ -0,0 +1,48 @@
+# Vim Pro - create a professional IDE from Vim in a minute
+### Plugin manager:
+* [pathogen.vim](https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim)
+### Basic Plugins list:
+* [Syntastic](https://github.com/scrooloose/syntastic) 
+* [YouCompleteMe](https://github.com/Valloric/YouCompleteMe) 
+* [NERDTree](https://github.com/scrooloose/nerdtree) 
+* [Taglist](https://github.com/vim-scripts/taglist.vim) 
+* [FZF](https://github.com/junegunn/fzf) 
+* [vim-airline](https://github.com/vim-airline/vim-airline) 
+* [vim-fugitive](https://github.com/tpope/vim-fugitive) 
+##### You can also choose:
+* [vim-statline (vim-airline replacement)](https://github.com/millermedeiros/vim-statline) 
+* [Ctrlp (fzf replacement)](https://github.com/ctrlpvim/ctrlp.vim) 
+### Additional packages:
+* silversearcher-ag
+* exuberant-ctags
+* python-dev 
+* python3-dev 
+* wget 
+* git
+### Custom commands:
+##### All modes
+ctrl+s - save 
+##### Normal mode
+F2 - Paste
+F3 - NERDTree
+F4 - Taglist
+F7 - Run silversearcher(ag)
+ctrl+x - save and exit
+ctrl+q - exit without saving
+ctrl+n - open a new tab
+ctrl+c + ctrl+l - close syntastic window
+wa - copy word to buffer
+wp - replace word with buffer (wa)
+ctrl+p - run Ctrlp (if you installed it)
+### Other:
++ Automatically removes trailing whitespaces on save
++ [Toggle auto-indenting for code paste(F2)](http://vim.wikia.com/wiki/Toggle_auto-indenting_for_code_paste)
+### Vim auto deployment on save
+Automatically upload files to a remote host whenever a change is saved in the Vim
+~/.vimrc:
+```bash
+autocmd BufWritePost * execute "!if [ $PWD == '/source/folder' ]; then 
+    /usr/bin/rsync -avz --update -e 'ssh -p 9966' --delete --exclude='.data' \
+    --exclude='migrations' --exclude='.env' --exclude='.git' --exclude='.idea' \
+    /source/folder root@remotehost.com:/destination/folder;fi"
+```
---
+++ vimpro.sh
@@ -0,0 +1,219 @@
+#!/bin/bash
+
+echo -n "Setup basic settings"
+while [[ "$ag" != "y" && "$ag" != "n" ]]; do
+    echo -n "Install xoria256 theme? (y/n)"
+    read line; done
+if [[ "$ag" == "y" ]]; then
+    if [[ -f ~/.vim/colors ]]; then
+        mkdir ~/.vim/colors
+        cp colors/xoria256.ini ~/.vim/colors/
+        echo -e "
+        set t_Co=256
+        colorscheme xoria256" >> ~/.vimrc
+ 
+echo -e "
+set t_Co=256
+colorscheme xoria256
+
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set expandtab
+set ai
+set autoindent
+set incsearch
+set nohlsearch
+set mousemodel=popup
+set termencoding=utf-8
+set guioptions-=T
+set mousehide
+set pastetoggle=<F2>
+set nocompatible
+
+set splitright
+set splitbelow
+set number
+
+function TrimEndLines()
+    let save_cursor = getpos('.')
+    :silent! %s#^\s\+\n#\r#g
+    :silent! %s#\s\+\n#\r#g
+    call setpos('.', save_cursor)
+endfunction
+
+au BufWritePre *.py call TrimEndLines()
+
+autocmd BufWinLeave *.* mkview
+autocmd BufWinEnter *.* silent loadview 
+
+augroup AutoSaveFolds
+  autocmd!
+    autocmd BufWinLeave * mkview
+    autocmd BufWinEnter * silent loadview
+  augroup END
+
+highlight OverLength ctermbg=red ctermfg=white guibg=#592929
+match OverLength /\%81v.\+/
+            
+if &history < 1000
+  set history=1000
+endif
+
+set hlsearch
+set incsearch
+set wildmenu
+set wildmode=list:longest,list:full
+set confirm
+set completeopt=menuone,longest
+set complete-=i
+
+nmap <C-x> :wq<CR>
+nmap <C-c><C-l> :lclose<CR>
+nmap <C-n> :tabedit<CR>
+noremap wa yiw
+noremap wp viwp
+
+nmap <C-q> :q!<CR>
+noremap <C-S> :w<CR>
+vnoremap <C-S> <C-C>:update<CR>
+inoremap <C-S> <C-O>:update<CR>
+" >> ~/.vimrc
+
+
+echo -n 'Install packages'
+sudo apt-get install -y python-dev python3-dev wget git exuberant-ctags
+
+echo -n 'Install pathogen'
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    wget https://tpo.pe/pathogen.vim -O ~/.vim/autoload/pathogen.vim
+    echo -e "
+        execute pathogen#infect()
+        syntax on
+        filetype plugin indent on" >> ~/.vimrc
+echo -n 'Done'
+
+echo -n 'Install syntastic'
+git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+echo -e "
+    set statusline+=%#warningmsg#
+    set statusline+=%{SyntasticStatuslineFlag()}
+    set statusline+=%*
+    let g:syntastic_always_populate_loc_list = 1
+    let g:syntastic_auto_loc_list = 1
+    let g:syntastic_check_on_open = 1
+    let g:syntastic_check_on_wq = 0
+    let g:syntastic_loc_list_height=5
+    let g:syntastic_python_checkers = ['flake8']
+    let g:syntastic_python_flake8_args = '--ignore=\"E501,E302,E261,E701,E241,E126,E127,E128,W801,E265,E303\"'" >> ~/.vimrc
+echo -n 'Done'
+
+echo -n 'Install  YouCompleteMe'
+cd ~/.vim/bundle/ && git clone https://github.com/Valloric/YouCompleteMe.git && \
+    cd YouCompleteMe && git submodule update --init --recursive && ./install.py --clang-completer
+echo -e "
+    au FileType python set omnifunc=pythoncomplete#Complete
+    let g:SuperTabDefaultCompletionType = 'context'
+    set completeopt=menuone,longest,preview
+    let g:ycm_auto_trigger = 0
+    let g:ycm_autoclose_preview_window_after_completion = 1" >> ~/.vimrc
+echo -n 'Done'
+
+echo -n 'Install NERDTree'
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+echo -e "
+\" NERDTree config
+map <F3> :NERDTreeToggle<CR>
+let NERDTreeIgnore=['.\.obj$', '.\.o$', '.\.so$', '.\.exe$', '.\.git$', '.\.swp$']
+\" Automatically quit vim if NERDTree is last and only buffer
+autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif
+
+function! NERDTreeHighlightFile(extension, fg, bg, guifg, guibg)
+ exec 'autocmd filetype nerdtree highlight ' . a:extension .' ctermbg='. a:bg .' ctermfg='. a:fg .' guibg='. a:guibg .' guifg='. a:guifg
+ exec 'autocmd filetype nerdtree syn match ' . a:extension .' #^\s\+.*'. a:extension .'$#'
+endfunction
+
+call NERDTreeHighlightFile('jade', 'green', 'none', 'green', '#151515')
+call NERDTreeHighlightFile('ini', 'yellow', 'none', 'yellow', '#151515')
+call NERDTreeHighlightFile('md', 'blue', 'none', '#3366FF', '#151515')
+call NERDTreeHighlightFile('py', 'lightblue', 'none', 'lightblue', '#0099ff')
+call NERDTreeHighlightFile('yml', 'yellow', 'none', 'yellow', '#151515')
+call NERDTreeHighlightFile('config', 'yellow', 'none', 'yellow', '#151515')
+call NERDTreeHighlightFile('conf', 'yellow', 'none', 'yellow', '#151515')
+call NERDTreeHighlightFile('json', 'yellow', 'none', 'yellow', '#151515')
+call NERDTreeHighlightFile('html', 'yellow', 'none', 'yellow', '#151515')
+call NERDTreeHighlightFile('styl', 'cyan', 'none', 'cyan', '#151515')
+call NERDTreeHighlightFile('css', 'cyan', 'none', 'cyan', '#151515')
+call NERDTreeHighlightFile('coffee', 'Red', 'none', 'red', '#151515')
+call NERDTreeHighlightFile('js', 'Red', 'none', '#ffa500', '#151515')
+call NERDTreeHighlightFile('php', 'Magenta', 'none', '#ff00ff', '#151515')
+" >> ~/.vimrc
+echo -n 'Done'
+
+echo -n 'Install Taglist'
+git clone https://github.com/vim-scripts/taglist.vim.git ~/.vim/bundle/taglist.vim
+echo -n 'Done'
+
+while [[ "$tagger" != "1" && "$tagger" != "2" ]]; do
+    echo -n "Install tagger:"
+    echo -n "1. Ctrlp"
+    echo -n "2. FZF"
+    read $tagger; done
+if [[ "$tagger" == "1" ]]; then
+    echo -n 'Install Ctrlp'
+    git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+    echo -e "
+    let g:ctrlp_extensions = ['tag']
+    nnoremap <C-o> :CtrlPTag<cr>
+    set runtimepath^=~/.vim/bundle/ctrlp.vim" >> ~/.vimrc
+    echo -n 'Done'
+else
+    echo -n 'Install FZF'
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+    git clone https://github.com/junegunn/fzf.vim.git ~/.vim/bundle/fzf.vim
+    echo "set rtp+=~/.fzf" >> ~/.vimrc
+    echo -n 'Done'
+fi
+
+while [[ "$ag" != "y" && "$ag" != "n" ]]; do
+    echo -n "Install silversearcher-ag? (y/n)"
+    read line; done
+if [[ "$ag" == "y" ]]; then
+    echo -n 'Installing silversearcher-ag'
+    sudo apt-get install silversearcher-ag
+    echo -e "nnoremap <F7> :Ag <C-R><C-W><CR>" >> ~/.vimrc
+    echo -n 'Done'
+fi
+
+while [[ "$line" != "1" && "$line" != "2" ]]; do
+    echo -n "Install status line:"
+    echo -n "1. vim-statline (simple)"
+    echo -n "2. vim-airline (complex)"
+    read line; done
+if [[ "$line" == "1" ]]; then
+    echo -n 'Installing vim-statline'
+    git clone https://github.com/millermedeiros/vim-statline.git ~/.vim/bundle/vim-statline
+    echo -n 'Done';
+else
+    echo -n 'Installing vim-airline'
+    git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+    echo -n 'Remember to run :Helptags to generate help tags!'
+    echo -e "
+    let g:airline#extensions#tabline#enabled = 1
+    let g:airline_theme='lucius'
+    let g:airline_section_z = '%p%%  \ue0a1 %l/%L Col:%c'
+    let g:airline_powerline_fonts = 0'
+    let g:airline_highlighting_cache = 1
+    let g:airline#extensions#syntastic#enabled = 0
+    let airline#extensions#tabline#tabs_label = ''
+    let airline#extensions#tabline#show_splits = 0
+    let g:airline#extensions#whitespace#checks = []
+    let g:airline_detect_spell=0" >> ~/.vimrc
+    echo -n 'Done';
+fi
+
+echo -n 'Install vim-fugitive'
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+vim -u NONE -c "helptags vim-fugitive/doc" -c q
+echo -n 'Done';
+
